e1a479eca6063cf8aae4234e7ce0f9110f0a5be0
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-31 22:47:48 +0000

    added requset to test it with Postman

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+//Add a route
+app.get('/', (req, res) => {
+  res.send('OkOkOk');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));

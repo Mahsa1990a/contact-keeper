670c5e8402f47f352fcfad67b18da00cea03acf7
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-01 15:06:37 +0000

    added basic info and router for users

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,13 @@
 // will have register route
+
+const express = require('express');
+const router = express.Router(); // so with this we dont need app.get(...) we do router.get(...) or router.post(...)
+
+// @ route            POST api/users
+// @ description      Register a user
+// @ access           Public
+router.post('/', (req, res) => {  //so here '/' means api/users because we defined it in main server.js
+  res.send('Register a User');
+}); 
+
+module.exports = router;

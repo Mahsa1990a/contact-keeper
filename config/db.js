1d697e0e213fe93fa5ad914bacb0617e545e0ba0
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-01 15:39:33 +0000

    added connectDB func

--- config/db.js
+++ config/db.js
@@ -4,3 +4,19 @@ const mongoose = require('mongoose');
 const config = require('config'); //we need to access that global variable that we created(default)
 const db = config.get('mongoURI');
 
+//mongoose returns promisses
+const connectDB = () => {
+  mongoose.connect(db, { //we added next line to not have warnings
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB contected'))
+  .catch(err => {
+    console.log("Error: ",err.message)
+    //exit with failure:
+    process.exit(1);
+  });
+};
+
+module.exports = connectDB;

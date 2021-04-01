3437bb161eb103a685b1897ea674e0afc72ffc16
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-01 15:48:32 +0000

    updated connectDB func with async await

--- config/db.js
+++ config/db.js
@@ -5,19 +5,38 @@ const config = require('config'); //we need to access that global variable that 
 const db = config.get('mongoURI');
 
 //mongoose returns promisses
-const connectDB = () => {
-  mongoose.connect(db, { //we added next line to not have warnings
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('MongoDB contected'))
-  .catch(err => {
+// const connectDB = () => {
+//   mongoose.connect(db, { //we added next line to not have warnings
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => console.log('MongoDB contected'))
+//   .catch(err => {
+//     console.log("Error: ",err.message)
+//     //exit with failure:
+//     process.exit(1);
+//   });
+// };
+
+// OR update connectDB with async await:
+const connectDB = async () => {
+  try {
+    await mongoose
+    .connect(db, { 
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB contected...');
+
+  } catch (err) {
     console.log("Error: ",err.message)
     //exit with failure:
     process.exit(1);
-  });
+  }
 };
 
 module.exports = connectDB;

eb06b098443fd438307b29d1808eb077d96bbcac
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-01 19:24:17 +0000

    added express-validator for validating our datas

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,34 @@
 
 const express = require('express');
 const router = express.Router(); // so with this we dont need app.get(...) we do router.get(...) or router.post(...)
+const { body, validationResult } = require('express-validator'); // add it for validation 
 
 const user = require('../models/User');
 
 // @ route            POST api/users
 // @ description      Register a user
 // @ access           Public
-router.post('/', (req, res) => {  //so here '/' means api/users because we defined it in main server.js
-  res.send('Register a User');
+router.post('/', 
+
+  //                          VALIDATE DATA:
+  // name must not be empty
+  body('name', 'Please add name').notEmpty(),
+  // email must be an email
+  body('email', 'Please include an email').isEmail(),
+  // password must be at least 5 chars long
+  body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+
+ (req, res) => {  //so here '/' means api/users because we defined it in main server.js
+
+  //res.send(req.body); // it will give us the data that's sent to the route(which is email, pass, name)
+
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() }); //will give us an arr of errors
+  }
+  
+  res.send('passed');
 }); 
 
 module.exports = router;

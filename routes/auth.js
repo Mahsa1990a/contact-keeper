133cd63fc85c717aa339dabe8aa4d664f033bebd
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-01 15:10:23 +0000

    added basic info and router for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,20 @@
-// Will have login, the authentication, and route to check the logged in user
+// Will have login, the authentication, and route to check the logged in userconst express = require('express');
+
+const express = require('express');
+const router = express.Router();
+
+// @ route            GET api/auth
+// @ description      Get logged in user
+// @ access           Private
+router.get('/', (req, res) => {  
+  res.send('Get logged in User');
+}); 
+
+// @ route            POST api/auth
+// @ description      Auth user and get token
+// @ access           Public
+router.post('/', (req, res) => {  
+  res.send('Log in user');
+});
+
+module.exports = router;

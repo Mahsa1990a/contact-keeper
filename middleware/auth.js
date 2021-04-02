bbaae81aa67f6b952e3e14232a4528406056d90d
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-02 17:05:59 +0000

    created func and complete it

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,5 +6,24 @@ const jwt = require('jsonwebtoken');
 const config = require('config'); //we need access to the secret
 
 module.exports = function(req, res, next) {
-  
+  // Get token from header
+  const token = req.header('x-ayth-token');
+
+  // Check if not token
+  if(!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // If there is a token, we need to verify it:
+  try {
+    const decodded = jwt.verify(token, config.get('jwtSecret')); //once it get verified the payload is gonna be put into decoded
+
+    // Now we wanna take the user out 
+    req.user = decodded.user;
+    next(); //then we call next to move on
+
+  } catch (err) {
+    res.status(401).send('Token is not valid');
+  }
+
 }

1cdf3d54eb62e8f3bc68c373fc244f053c097d7c
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-02 16:58:44 +0000

    required whatever we needed and create fun

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,10 @@
 // middleware is a function that has access to the request and response cycle and the req and res object
 // Everytime we hit an endpoint we can fire off this middleware and we just wanna check to see if there is a token in the header so:
 
-//we need to bring:
+//we need to bring to verify the token:
 const jwt = require('jsonwebtoken');
+const config = require('config'); //we need access to the secret
+
+module.exports = function(req, res, next) {
+  
+}

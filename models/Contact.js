787767d98f17368ea3b4120c86c9da744da37270
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-03 14:13:36 +0000

    created Contacts model almost same a User model

---
+++ models/Contact.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = mongoose.Schema({
+
+  //  We need to create a relationship between contacts and users because each users has their own set of contacts
+  user: {
+    type: mangoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    // unique: true  email doesnt need to be unique, because it's a contact
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now //will out current date automatically
+  }
+
+});
+
+module.exports =  mongoose.model('contact', ContactSchema);

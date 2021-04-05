1ced3a02c8e4b757c991e87d7ad2da54b9b81643
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-05 23:02:07 +0000

    import ContactContext

--- client/src/components/contacts/ContactForm.jsx
+++ client/src/components/contacts/ContactForm.jsx
@@ -1,8 +1,11 @@
 // This ContactForm is gonna be use to add and update contacts
 
-import React, { useState } from 'react'; //we import useState because since it's a form we do need some component level state for each field
+import React, { useContext, useState } from 'react'; //we import useState because since it's a form we do need some component level state for each field
+import ContactContext from "../../context/contact/contactContext";
 
 const ContactForm = () => {
+
+  const contactContext = useContext(ContactContext); //we need a func add contact from it
 
   const [contact, setContact ] = useState({
     //defualt of contact(obj):

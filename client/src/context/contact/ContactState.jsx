55c6d708d2f37f68492e6118b86655ab53c9011b
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-05 17:19:24 +0000

    import from type

--- client/src/context/contact/ContactState.jsx
+++ client/src/context/contact/ContactState.jsx
@@ -2,3 +2,7 @@ import React, { useReducer } from 'react'; // useReducer hook for accessing stat
 import uuid from 'uuid'; // it's only for generate random id
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
+import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from '../types';
+
+
+

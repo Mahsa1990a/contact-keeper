2a06f9398c48b37d8844dd855bd663e0a56ac698
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-05 20:26:01 +0000

    added onChange func

--- client/src/components/contacts/ContactForm.jsx
+++ client/src/components/contacts/ContactForm.jsx
@@ -14,6 +14,10 @@ const ContactForm = () => {
 
   const { name, email, phone, type } = contact;
 
+  const onChange = (e) => {
+    setContact({...contact, [e.target.name]: e.target.value });
+  }
+
   return (
     <form>
       <h2 className="text-primary">Add Contact</h2>
@@ -23,6 +27,9 @@ const ContactForm = () => {
       <h5>Contact Type</h5>
       <input type="radio" name="type" value="personal" checked={type === 'personal'}/> Personal{' '}
       <input type="radio" name="type" value="professional" checked={type === 'professional'}/> Professional
+      <div>
+        <input type="submit" value="Add Contact" className="btn btn-primary btn-block"/>
+      </div>
     </form>
   )
 }

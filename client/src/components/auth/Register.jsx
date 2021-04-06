7ae25a541f6f7a75c68a737f39c1289d1e6d4440
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-06 19:19:18 +0000

    defined onSubmit

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -16,10 +16,16 @@ const Register = () => {
     setUser({ ...user, [e.target.name]: e.target.value })
   };
 
+  const onSubmit = e => {
+    e.preventDefault();
+
+    console.log('Register Submit ...')    
+  }
+
   return (
     <div className='form-container'>
       <h1>Account <span className="text-primary">Register</span></h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
           <input type="text" name="name" value={name} onChange={onChange} />

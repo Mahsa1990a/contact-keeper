3e866cc9584e678dddea97c03cd4f016e273898d
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-06 20:21:06 +0000

    if there is alert, loop through it and return that jsx

--- client/src/components/layout/Alerts.jsx
+++ client/src/components/layout/Alerts.jsx
@@ -4,13 +4,16 @@ import AlertContext from '../../context/alert/alertContext';
 function Alerts() {
 
   const alertContext = useContext(AlertContext);
-  const {} = alertContext;
-  
+  const { alerts } = alertContext;
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    // We want to check to see if there is any alerts in the arr
+    alerts.length > 0 && alerts.map(alert => (
+      <div key={alert.id} className={`alert alert-${alert.type}`} >
+        <i className="fas fa-info-circle"></i> {alert.msg}
+      </div>
+    ))
+  );
+};
 
 export default Alerts;

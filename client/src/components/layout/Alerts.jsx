ddfd9ecb9e705e09b8dadeafff1fcb281d90fba8
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-06 20:16:32 +0000

    added AlertContext

---
+++ client/src/components/layout/Alerts.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+
+function Alerts() {
+
+  const alertContext = useContext(AlertContext);
+  const {} = alertContext;
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Alerts;

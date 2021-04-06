8d04956a1949ba72d1a5046af3ca04ef2a012b75
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-06 15:28:28 +0000

    added useEffect

--- client/src/components/contacts/ContactFilter.jsx
+++ client/src/components/contacts/ContactFilter.jsx
@@ -1,12 +1,19 @@
-import React, { useContext, useRef } from 'react'; //useRef is a hook
+import React, { useContext, useRef, useEffect } from 'react'; //useRef is a hook
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactFilter = () => {
 
   const contactContext = useContext(ContactContext);
-  const { filterContacts, clearFilter } = contactContext;
+  const { filterContacts, clearFilter, filtered } = contactContext;
 
   const text = useRef(''); //initialize ref value
+
+  // If filtered is equal to null, I want the value to be empty, don't want to keep text inside of it
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = '';
+    }
+  }, []);
 
   const onChange = (e) => {
     // we can get the actual value by input: text.current.value

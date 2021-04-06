0a5b4ffdc917d27fc327868622e24f9365b49994
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-06 15:20:46 +0000

    added onChange func and initialized ref value

--- client/src/components/contacts/ContactFilter.jsx
+++ client/src/components/contacts/ContactFilter.jsx
@@ -5,6 +5,13 @@ const ContactFilter = () => {
 
   const contactContext = useContext(ContactContext);
 
+  const text = useRef(''); //initialize ref value
+
+  const onChange = (e) => {
+    // we can get the actual value by input:
+    text.current.value
+  };
+
   return (
     <form>
       <input ref={text} type="text" placeholder="Filter Contacts ..." onChange={onChange} />

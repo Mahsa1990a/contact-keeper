8552f4b34f13aea37c7a8c661ba2d9c195246514
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-06 15:08:38 +0000

    added filtered into value

--- client/src/context/contact/ContactState.jsx
+++ client/src/context/contact/ContactState.jsx
@@ -84,6 +84,7 @@ const ContactState = (props) => {
       value = {{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,

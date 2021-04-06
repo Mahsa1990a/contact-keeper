4f27c244e16381b90dd928814cdc6118ff18d0e3
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-06 19:15:01 +0000

    added for email pass, ... as well

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -24,6 +24,18 @@ const Register = () => {
           <label htmlFor="name">Name</label>
           <input type="text" name="name" value={name} onChange={onChange} />
         </div>
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" value={password} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password2">Confirm Password</label>
+          <input type="password" name="password2" value={password2} onChange={onChange} />
+        </div>
       </form>
     </div>
   )

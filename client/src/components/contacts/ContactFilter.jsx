f3b52be48a73a42ebfe2b5c852325e1e46195765
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-06 15:24:11 +0000

    added if statement for onChange func

--- client/src/components/contacts/ContactFilter.jsx
+++ client/src/components/contacts/ContactFilter.jsx
@@ -4,12 +4,17 @@ import ContactContext from '../../context/contact/contactContext';
 const ContactFilter = () => {
 
   const contactContext = useContext(ContactContext);
+  const { filterContacts, clearFilter } = contactContext;
 
   const text = useRef(''); //initialize ref value
 
   const onChange = (e) => {
-    // we can get the actual value by input:
-    text.current.value
+    // we can get the actual value by input: text.current.value
+    if (text.current.value !== '') {
+      filterContacts(e.target.value); //e.target.value => which is actual text
+    } else {
+      clearFilter();
+    }
   };
 
   return (

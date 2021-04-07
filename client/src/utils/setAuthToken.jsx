45c5917ff18d3b6f50661a18dce5083d307ab513
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-07 16:43:31 +0000

    added setAuthToken to check if token is passed or not

---
+++ client/src/utils/setAuthToken.jsx
@@ -0,0 +1,15 @@
+// We'll check to see if a token is passed in, if it is, then we're gonna set it to the main global header 
+// If not, we'll delete it from global header
+
+import React from 'react';
+import axios from 'axios';
+
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-auth-token'];
+  }
+}
+
+export default setAuthToken;

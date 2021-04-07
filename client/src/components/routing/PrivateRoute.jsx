517c9085cb6a9d5bc14e51243f76479fb2a25d30
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-07 19:09:53 +0000

    added Route, Redirect, Component

--- client/src/components/routing/PrivateRoute.jsx
+++ client/src/components/routing/PrivateRoute.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 // We need AuthContext to see if we're logged in or not:
 import AuthContext from '../../context/auth/authContext';
 
@@ -8,9 +9,14 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
   const { isAuthenticated, loading } = authContext;
 
   return (
-    <div>
+    <Route {...rest} render={props => !isAuthenticated && !loading ? (
+        <Redirect to='/login'/> 
+      ) : (
+        <Component {...props} />
+      )} 
+    />
       
-    </div>
+    
   )
 }
 

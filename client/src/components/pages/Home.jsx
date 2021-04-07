050de778740b37b616d4562ffa7aa60505b41e54
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-07 17:20:34 +0000

    added useEffect

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../contacts/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+
+  const authContext = useContext(AuthContext);
+  const { loadUser } = authContext;
+
+  useEffect(() => {
+    loadUser(); //It will look at the token, ot will hit the backend, validate it and put the user into state
+    
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className='grid-2'>
       <div>

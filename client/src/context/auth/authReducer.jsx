b56f443c8a976bc8d37e2c44e6d5c41b1951f08c
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-07 16:34:05 +0000

    defined case for USER_LOADED

--- client/src/context/auth/authReducer.jsx
+++ client/src/context/auth/authReducer.jsx
@@ -13,6 +13,13 @@ import {
 // eslint-disable-next-line
 export default (state, action) => {
   switch(action.type) {
+    case USER_LOADED:
+    return {
+      ...state,
+      isAuthenticated: true,
+      loading: false,
+      user: action.payload
+    };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {

6cca35d4eb4e22b3b7e5994c460fb82d8cccb33e
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-07 18:31:28 +0000

    added login-fail case

--- client/src/context/auth/authReducer.jsx
+++ client/src/context/auth/authReducer.jsx
@@ -31,6 +31,7 @@ export default (state, action) => {
       };
     case REGISTER_FAIL: 
     case AUTH_ERROR: //it does same thing as REGISTER_FAIL
+    case LOGIN_FAIL:
     localStorage.removeItem('token');
     return {
       ...state,

0d7572936a761c16b0daf7838b1f5297ac3f64e3
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-07 18:51:36 +0000

    added guestLinks

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -19,6 +19,17 @@ const Navbar = ({ title, icon }) => {
     </Fragment>
   );
 
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
       <h1>
@@ -27,18 +38,7 @@ const Navbar = ({ title, icon }) => {
         </Link>
       </h1>
       <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
+        
       </ul>
     </div>
   )

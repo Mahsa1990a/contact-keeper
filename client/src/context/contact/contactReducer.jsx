25eddf381108903b070dbdaacb281f28851ab306
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-08 15:32:36 +0000

    added case for GET_CONTACTS

--- client/src/context/contact/contactReducer.jsx
+++ client/src/context/contact/contactReducer.jsx
@@ -1,8 +1,24 @@
-import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from '../types';
+import { 
+  GET_CONTACTS,
+  ADD_CONTACT, 
+  DELETE_CONTACT, 
+  SET_CURRENT, 
+  CLEAR_CURRENT, 
+  UPDATE_CONTACT, 
+  FILTER_CONTACTS,
+  CLEAR_CONTACTS, 
+  CLEAR_FILTER 
+} from '../types';
 
 // eslint-disable-next-line
 export default (state, action) => {
   switch(action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false
+      }
     case ADD_CONTACT:
       return {
         ...state,

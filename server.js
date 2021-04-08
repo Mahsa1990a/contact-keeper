35c34c7303965637c01177a06dfe9f49c190905f
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-08 19:17:01 +0000

    updated server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -10,15 +11,26 @@ connectDB();
 app.use(express.json({ extended: false })); //by doing this we can accept body data
 
 //Add a route
-app.get('/', (req, res) => {
-  //it's gonna be a json API so:
-  res.json({ msg: 'Welcome to the ContactKeeper API.....' });
-});
+// app.get('/', (req, res) => {
+//   //it's gonna be a json API so:
+//   res.json({ msg: 'Welcome to the ContactKeeper API.....' });
+// });
 
 // Define our routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // Now create route:
+  app.get('*', (req, res) => { // '*' means anything that is not line 19,20,21
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })   
+}
 
 const PORT = process.env.PORT || 5000;
 

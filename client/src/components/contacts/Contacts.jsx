56f46c020cdf1fbff27413a1fe719627096c4f25
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-08 15:38:44 +0000

    added useEffect for calling getContacts

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment } from 'react';
+import React, { useContext, Fragment, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group'; // adding animation
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
@@ -7,11 +7,19 @@ function Contacts() {
 
   const contactContext = useContext(ContactContext); // So now we have access to any access associated with contactContext
 
-  const { contacts, filtered } = contactContext; // pull out hard coded contacts from ContactState.jsx
+  const { contacts, filtered, getContacts, loading } = contactContext; // pull out hard coded contacts from ContactState.jsx
+
+  // We wanna call getContacts within useEffect when this component loads
+  useEffect(() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, []); //we just run it at the begining so we pass []
+
 
   if (contacts.length === 0) { // If contact is empty
     return <h4>Please Add a Contact</h4>
   } 
+
 
   return (
     <Fragment>

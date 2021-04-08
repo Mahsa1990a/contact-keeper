1de18af0b3b0bb2ba224d022dbfa7b4657155186
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-08 15:34:43 +0000

    added loading: false just for the basic CRUDs

--- client/src/context/contact/contactReducer.jsx
+++ client/src/context/contact/contactReducer.jsx
@@ -22,18 +22,21 @@ export default (state, action) => {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload]
+        contacts: [...state.contacts, action.payload],
+        loading: false
       };
     case UPDATE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
+        contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact),
+        loading: false
       };
     case DELETE_CONTACT:
       return {
         ...state,
         // state.contacts which is the current arr and then filter it => evaluate and will return all contacts that are not the current id
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+        contacts: state.contacts.filter(contact => contact.id !== action.payload),
+        loading: false
       };
     case SET_CURRENT:
       return {

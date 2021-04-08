fb877668f899b75fea9262611f0590f8b41e7b4d
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-04-08 16:05:53 +0000

    in case for DELETE contact just change id into _id

--- client/src/context/contact/contactReducer.jsx
+++ client/src/context/contact/contactReducer.jsx
@@ -35,7 +35,7 @@ export default (state, action) => {
       return {
         ...state,
         // state.contacts which is the current arr and then filter it => evaluate and will return all contacts that are not the current id
-        contacts: state.contacts.filter(contact => contact.id !== action.payload),
+        contacts: state.contacts.filter(contact => contact._id !== action.payload),
         loading: false
       };
     case CLEAR_CONTACTS:
